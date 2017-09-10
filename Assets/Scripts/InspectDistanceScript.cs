da5abb2467359b7f761b810cd4c5f3722aa2256d
Cks <pikacusheng@gmail.com>
2017-09-11 02:31:07 +0800

    Nav Mesh added, auto pilot added

---
+++ Assets/Scripts/InspectDistanceScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InspectDistanceScript : MonoBehaviour {
+
+	public GameObject player;
+	public float range = 20f;
+
+	float distance;
+	Renderer rend;
+	Color originalColor;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+		originalColor = rend.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(transform.position, player.transform.position);
+		if(distance <= range)
+		{
+			rend.material.color = Color.blue;
+//			if(Input.GetKeyDown(KeyCode.A)) /!testing
+//				{
+//					PathFinderManager.Instance.startMove = true;
+//					PathFinderManager.Instance.goal = this.transform;	
+//				}
+			DetectTap();
+		}
+		else
+		{
+			rend.material.color = originalColor;
+		}
+
+	}
+
+	void DetectTap()
+	{
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			Ray rayCast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+			RaycastHit castHit;
+			if(Physics.Raycast(rayCast, out castHit))
+			{
+				if(castHit.collider.CompareTag("InspectableObjects"))
+				{
+					PathFinderManager.Instance.goal = castHit.collider.gameObject.transform;
+					PathFinderManager.Instance.startMove = true;
+				}
+			}	
+		}
+	}
+}
---
+++ Assets/Scripts/PathFinderManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AI;
+using UnityEngine.AI;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PathFinderManager : MonoBehaviour {
+
+	public static PathFinderManager Instance;
+	public GameObject player;
+	public Transform goal;
+	public bool startMove;
+	public float remainingPos;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+		agent = player.GetComponent<NavMeshAgent>();
+		agent.stoppingDistance = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(startMove)
+		{
+			agent.destination = goal.position;
+			agent.isStopped = false;
+			if(CrossPlatformInputManager.GetAxis("Vertical") != 0 || CrossPlatformInputManager.GetAxis("Horizontal") != 0)
+			{
+				StopMoving();
+			}
+			else if(!agent.pathPending)
+			{
+				remainingPos = Vector3.Distance(player.transform.position,goal.transform.position);
+
+				if(remainingPos <= agent.stoppingDistance)
+				{
+					StopMoving();	
+				}
+			}
+		}
+	}
+
+	void StopMoving()
+	{
+		agent.isStopped = true;
+		goal = null;
+		startMove = false;	
+	}
+}

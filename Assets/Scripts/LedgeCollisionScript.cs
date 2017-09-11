4b7d3c0c31c2eb3de12a8884bdfceb9b139af330
TerrenceChan <roxes2112@yahoo.com>
2017-09-11 11:11:04 +0800

    Cliff Game

---
+++ Assets/Scripts/LedgeCollisionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LedgeCollisionScript : MonoBehaviour
+{
+
+	PlayerMovementScript player;
+
+	void Start ()
+	{
+		player = FindObjectOfType<PlayerMovementScript> ();
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	//player holds on ledge when colliding with objects with tag "Ledge"
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Ledge") {
+			Debug.Log ("Touch The Ledge");
+			player.isOnLedge = true;
+		}
+	}
+}

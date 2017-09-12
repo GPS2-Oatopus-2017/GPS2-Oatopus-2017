68bd3811582639b337e1e70f7bf570e3ec07e672
FattyMieo <fong2009@live.com.my>
2017-09-13 00:49:33 +0800

    jp v2.0

---
+++ Assets/Scripts/CloseWindow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public enum WindowType
+{
+	first = 0,
+	second,
+	total
+}
+public class CloseWindow : MonoBehaviour {
+	public WindowType myType;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(myType == WindowType.first)
+		{
+			if
+			(
+					CrossPlatformInputManager.GetAxis("Vertical") != 0 ||
+					CrossPlatformInputManager.GetAxis("Horizontal") != 0 ||
+					CrossPlatformInputManager.GetButtonDown("Jump")
+			)
+			{
+				this.gameObject.SetActive(false);
+			}
+		}
+		else if(myType == WindowType.second)
+		{
+			if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				this.gameObject.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PathFinderManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AI;
+using UnityEngine.AI;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PathFinderManager : MonoBehaviour {
+
+	public static PathFinderManager Instance;
+	public GameObject player;
+	public GameObject confirmWindow;
+	public GameObject goal;
+	public InspectDistanceScript inspectObjects;
+	public bool startMove;
+	public float remainingPos;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		confirmWindow.SetActive(false);
+		Instance = this;
+		agent = player.GetComponent<NavMeshAgent>();
+		agent.enabled = false;
+		agent.stoppingDistance = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(startMove)
+		{
+			agent.enabled = true;
+			agent.destination = goal.transform.position;
+			inspectObjects = goal.GetComponent<InspectDistanceScript>();
+			agent.isStopped = false;
+			if(CrossPlatformInputManager.GetAxis("Vertical") != 0 || CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetButtonDown("Jump"))
+			{
+				StopMoving();
+				inspectObjects = null;
+			}
+			else if(!agent.pathPending)
+			{
+				remainingPos = Vector3.Distance(player.transform.position,goal.transform.position);
+				if(remainingPos <= agent.stoppingDistance)
+				{
+					//player.transform.LookAt(goal.transform.position);
+					StopMoving();
+					confirmWindow.SetActive(true);
+					Debug.Log("on");
+				}
+			}
+		}
+	}
+
+	void StopMoving()
+	{
+		agent.isStopped = true;
+		agent.enabled = false;
+		//goal = null;
+		startMove = false;	
+	}
+
+	public void Inspected()
+	{
+		inspectObjects.isInspected = true;
+		goal = null;
+		inspectObjects = null;
+	}
+}
